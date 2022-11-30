47cee2f3ae30130f88698afaecee236e825c6366
MayaraWeinsteinMay <programacaomayaraw@gmail.com>
2022-11-30 14:35:08 -0300

    Bl.P.React3

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/estaticos/navbar/Navbar';
+import Footer from './components/estaticos/footer/Footer';
+import Home from './paginas/home/Home';
+
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <Home />
+    <Footer />
+    </>
+  );
+}
+
+export default App;
